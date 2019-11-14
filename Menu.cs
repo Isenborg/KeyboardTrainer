0354985beec6c3dd660f14a4fa56f7e63482f71e
akaMackiee <46831591+akaMackiee@users.noreply.github.com>
2019-11-14 13:13:21 +0100

    Lade till några nya klasser

---
+++ Menu.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace KeyboardTrainer
+{
+    class Menu
+    {
+        public void run()
+        {
+            System.Console.WriteLine("hello world");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace KeyboardTrainer
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            var menu = new Menu();
+            menu.run();
+        }
     }
 }
